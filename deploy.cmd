02400f4bfb7a5f2a9f6e60502a334bc039ea400a
Adrian Russell <adrian.f.russell@gmail.com>
2016-11-13 16:21:45 +1100

    add deploy.cmd

---
+++ deploy.cmd
@@ -0,0 +1,34 @@
+::Deployment
+echo Handling node.js deployment.
+ 
+:: 1. KuduSync
+echo Begin Sync and more
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"  
+  IF !ERRORLEVEL! NEQ 0 goto error
+ ) 
+
+echo Sync complete
+  
+:: 2. Select node version
+call :SelectNodeVersion
+ 
+:: 3. Install npm packages
+echo Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+echo Install npm complete
+ 
+:: 4. Build the webclient
+echo run npm build
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! run build
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+echo run npm build complete
