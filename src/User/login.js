91ffebab864cf7063159a294eccb40c517af98aa
Adrian Russell <adrian.f.russell@gmail.com>
2017-06-01 21:52:51 +1000

    wireup login.js to token api.

--- src/User/login.js
+++ src/User/login.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import ApiUrl from '.././apiUrl';
+import Cookie from '.././cookie';
 
 class Login extends React.Component {
       constructor(props) {
     super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);    
+    this.handleSubmit = this.handleSubmit.bind(this);      
+     
   }
 
   render() {
@@ -15,7 +22,7 @@ class Login extends React.Component {
           <section className="login-form">
             <div><p>Note: Site will be secured in 90 days</p></div>
             <form method="post" action="#" role="login" onSubmit={this.handleSubmit}>
-              <input type="text" name="userName" placeholder="User Name" required className="form-control input-lg" />
+            <input type="text" name="userName" placeholder="User Name" required className="form-control input-lg" value={this.state.value} onChange={this.handleChange}/>
             {/* Add in password at a later date 
               <input type="password" className="form-control input-lg" id="password" placeholder="Password" required />*/}
               <div className="pwstrength_viewport_progress" />
@@ -30,11 +37,37 @@ class Login extends React.Component {
       </div>
       </div>  
     );
-  
+  }
+    handleChange(event) {
+    this.setState({value: event.target.value});
+    
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ');
+
+    var baseApiUrl = new ApiUrl().getBase();
+    
+    var payload = {
+      UserName: this.state.value,
+    };
+
+    fetch(baseApiUrl + '/api/token',
+    {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'      
+      },
+      body: JSON.stringify( payload )
+    })
+  .then(function(res){ return res.json(); })
+  .then(function(data){ 
+    if (data.OperationSuccess === 'true')
+    {
+      new Cookie().createCookie('BATTLETECHSessionKey',data.Token,30);
+    }
+    alert( JSON.stringify( data ) )
+   })
+
     event.preventDefault();
   }
 
